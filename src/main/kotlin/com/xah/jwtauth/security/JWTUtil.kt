ab1fdd7ff2e643af8da76fc2ae568ffd0da2301c
00XAH00 <pxaxulin@gmail.com>
2023-05-04 13:30:58 +0300

    add jwt util

---
+++ src/main/kotlin/com/xah/jwtauth/security/JWTUtil.kt
@@ -0,0 +1,46 @@
+package com.xah.jwtauth.security
+
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class JWTUtil {
+
+    @Value("\${jwtAuth.secretToken}")
+    private lateinit var secretToken: String
+    @Value("\${jwtAuth.tokenExpireMilliseconds}")
+    private var tokenExpireMilliseconds: Int = 0
+
+    fun generateToken(userLogin: String): String {
+        val claims = Jwts.claims().setSubject(userLogin)
+        return Jwts.builder()
+            .setClaims(claims)
+            .setExpiration(Date(System.currentTimeMillis() + tokenExpireMilliseconds))
+            .signWith(Keys.hmacShaKeyFor(secretToken.toByteArray()))
+            .compact()
+    }
+
+    fun getUsernameFromToken(token: String): String {
+        return Jwts.parserBuilder().setSigningKey(secretToken).build().parseClaimsJws(token).body.subject
+    }
+
+    fun validateToken(token: String): Boolean {
+        val claims = Jwts.parserBuilder().setSigningKey(secretToken).build().parseClaimsJws(token).body
+        val expiration = claims.expiration
+        return expiration.after(Date())
+    }
+
+    fun extractToken(request: HttpServletRequest): String? {
+        val authHeader = request.getHeader("Authorization")
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            return authHeader.substring(7)
+        }
+        return null
+    }
+
+
+}
