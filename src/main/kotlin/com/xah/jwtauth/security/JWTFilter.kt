2c91915ca423182c83f21f624340dd232500da8e
00XAH00 <pxaxulin@gmail.com>
2023-05-04 13:30:37 +0300

    add jwt filter

---
+++ src/main/kotlin/com/xah/jwtauth/security/JWTFilter.kt
@@ -0,0 +1,28 @@
+package com.xah.jwtauth.security
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+
+
+@Component
+class JWTFilter(
+    private val jwtTokenUtil: JWTUtil,
+    private val userDetailsService: UserDetailsService
+) : OncePerRequestFilter() {
+
+    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
+        val token = jwtTokenUtil.extractToken(request)
+        if (token != null && jwtTokenUtil.validateToken(token)) {
+            val userDetails = userDetailsService.loadUserByUsername(jwtTokenUtil.getUsernameFromToken(token))
+            val authentication = UsernamePasswordAuthenticationToken(userDetails, null, userDetails.authorities)
+            SecurityContextHolder.getContext().authentication = authentication
+        }
+        filterChain.doFilter(request, response)
+    }
+}
