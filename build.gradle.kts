fc7e05b20175e613263add4dc2ca412d680d0af8
00XAH00 <pxaxulin@gmail.com>
2023-05-04 13:29:24 +0300

    add spring security boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
+    implementation ("org.springframework.boot:spring-boot-starter-security")
     implementation("io.jsonwebtoken:jjwt-api:0.11.2")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
